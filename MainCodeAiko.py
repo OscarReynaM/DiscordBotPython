6a4e45460a70b7713f7ea0a1c0d17d6628aae46d
Oscar A Reyna Monreal <69470999+OscarReynaM@users.noreply.github.com>
2020-11-05 23:20:56 -0600

    Update MainCodeAiko.py

--- MainCodeAiko.py
+++ MainCodeAiko.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import random
 
 client = discord.Client()
+counter = 0
+id = 000000000000000000
+
 #client = commands.Bot(command_prefix='=')
 
 @client.event
@@ -24,7 +27,7 @@ async def on_message(message):
         await message.channel.send(embed=myembed)
     
     if (message.author.id == 324383693248659456):
-        valor = random.randint(0,5)
+        valor = random.randint(0,7)
         
         if (valor == 0):
             size = message.content
@@ -55,6 +58,28 @@ async def on_message(message):
             
         if (valor == 6):
             await message.channel.send('Ya deja de ser negro')
+            
+        if (valor == 7):
+            await message.channel.send('Hola Ray, nomas queria tomarme este minuto para decir que te quiero, eres una gran persona, si estas pasando por un mal momento en tu vida sabe que tienes amigos con quien platicar y se tomarian el tiempo para escucharte. Nos sentimos bendecidos porque te tenemos alrededor de nosotros y esperamos que sigas ahi. Hemos pasado por muchos momentos de risa y estamos agradecidos por ello. Eres la persona que se puede contar para tener unas buenas risas. Tambien vales verga.')
+            
+@client.event
+async def on_message(message): #funcion para devolver mucho texto a una persona despues de 15 mensajes
+    global id
+    global counter
+                        
+    if (message.author.id == id):
+        counter += 1
+    else:
+        counter = 1
+    
+    if (counter >= 15):
+        await message.channel.send('Mucho Texto')
+        counter = 1
+        
+    id = message.author.id
+
+            
+    
     
 
 client.run('NzIwMjc2MzMzNzgyODkyNjM2.XuDnnA.cBKj3PzfhKatqabsZwooW69qhyk')
@@ -63,3 +88,4 @@ client.run('NzIwMjc2MzMzNzgyODkyNjM2.XuDnnA.cBKj3PzfhKatqabsZwooW69qhyk')
 #Id de jesusu  166675689506996234
 #Id de paco  210469827972694016
 #Id de Oscar  129103410912952320
+#Id de Mau  289032318482382848
