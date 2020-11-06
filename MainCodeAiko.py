9e8a1187b798531b75924343d11842954229a6e1
Oscar A Reyna Monreal <69470999+OscarReynaM@users.noreply.github.com>
2020-11-05 21:34:40 -0600

    Update MainCodeAiko.py

--- MainCodeAiko.py
+++ MainCodeAiko.py
@@ -24,20 +24,42 @@ async def on_message(message):
         await message.channel.send(embed=myembed)
     
     if (message.author.id == 324383693248659456):
-        size = message.content
-        output = ""
-        for i in range(len(size)):
-            for letter in size[i].lower():
-                if (random.randint(0,100))% 2 == 0:   
-                    output += letter.lower()
-                else:
-                    output += letter.upper()
+        valor = random.randint(0,5)
         
-        await message.channel.send(output)
+        if (valor == 0):
+            size = message.content
+            output = ""
+            for i in range(len(size)):
+                for letter in size[i].lower():
+                    if (random.randint(0,100))% 2 == 0:   
+                        output += letter.lower()
+                    else:
+                        output += letter.upper()
+            
+            await message.channel.send(output)
+            
+        if (valor == 1):
+            await message.channel.send('Ya callate we')
+            
+        if (valor == 2):
+            await message.channel.send('Que es puto dicen')
+            
+        if (valor == 3):
+            await message.channel.send('Mejor ponte a traducir')
+            
+        if (valor == 4):
+            await message.channel.send('Donde esta la novela?')
+            
+        if (valor == 5):
+            await message.channel.send('Mejor come un pene')
+            
+        if (valor == 6):
+            await message.channel.send('Ya deja de ser negro')
     
 
 client.run('NzIwMjc2MzMzNzgyODkyNjM2.XuDnnA.cBKj3PzfhKatqabsZwooW69qhyk')
 
-#Id de ray324383693248659456
-# Id de jesusu 166675689506996234
-#id de paco 210469827972694016
+#Id de ray  324383693248659456
+#Id de jesusu  166675689506996234
+#Id de paco  210469827972694016
+#Id de Oscar  129103410912952320
