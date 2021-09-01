f7f6610443b5ebadca23a0d875e9350297dbb14e
Oscar A Reyna Monreal <69470999+OscarReynaM@users.noreply.github.com>
2021-09-01 17:59:29 -0500

    Update MainCodeAiko.py

--- MainCodeAiko.py
+++ MainCodeAiko.py
@@ -1,16 +1,87 @@
 import discord
+import youtube_dl
+import os
+from os import system
 from discord.ext import commands
+from discord.ext.commands import Bot
+from discord import FFmpegPCMAudio
+from discord.utils import get
 import random
 
-client = discord.Client()
+client = commands.Bot(command_prefix='=')
+#client = discord.Client()
 counter = 0
+total = 0
 id = 000000000000000000
 
-#client = commands.Bot(command_prefix='=')
 
 @client.event
 async def on_ready():
     print('Logged on as', client.user)
+
+@client.event
+async def on_disconnect(self):
+    print ("Aiko going offline.")
+    
+@client.command(aliases=['f', 'F'])
+async def _f(ctx):
+    global total
+    total = total + 1
+    embed = discord.Embed(description = f'{ctx.author.mention} has paid their respects.')
+    embed.set_footer(text= ' Total: ' + str(total))
+    
+    await ctx.send(embed=embed)    
+
+
+#Codigo para unirse al canal y poner musica, sigue en desarrollo
+
+@client.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    if not channel:
+        await ctx.send('No estas en un VC :p')
+        return
+    
+    await channel.connect()
+    
+@client.command(pass_context=True, brief="This will play a song 'play [url]'", aliases=['p'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Espera a que termine el rolon")
+        return
+    
+    await ctx.send("Preparando rolon, dame unos segundos")
+    print("Alguien puso un rolon, deja lo pongo.....")
+    
+    voice = get(client.voice_clients, guild=ctx.guild)
+    ydl_opts = {
+        'default_search': 'auto',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, 'song.mp3')
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    voice.volume = 100
+    voice.is_playing()
+
+
+
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
     
 
 @client.event
@@ -21,13 +92,9 @@ async def on_message(message):
 
     if message.content == 'ping':
         await message.channel.send('pong')
-        
-    if (message.content == 'F' or message.content == 'f' or message.content == '=F' or message.content == '=f'):
-        myembed = discord.Embed(description = f'{message.author.mention} has paid their respects.')
-        await message.channel.send(embed=myembed)
     
     if (message.author.id == 324383693248659456):
-        valor = random.randint(0,7)
+        valor = random.randint(0,10)
         
         if (valor == 0):
             size = message.content
@@ -42,28 +109,22 @@ async def on_message(message):
             await message.channel.send(output)
             
         if (valor == 1):
-            await message.channel.send('Ya callate we')
+            await message.channel.send('Eres el mejor')
             
         if (valor == 2):
-            await message.channel.send('Que es puto dicen')
-            
-        if (valor == 3):
             await message.channel.send('Mejor ponte a traducir')
             
-        if (valor == 4):
+        if (valor == 3):
             await message.channel.send('Donde esta la novela?')
             
+        if (valor == 4):
+            await message.channel.send('Ya desayunaste?')
+            
         if (valor == 5):
-            await message.channel.send('Mejor come un pene')
-            
-        if (valor == 6):
-            await message.channel.send('Ya deja de ser negro')
-            
-        if (valor == 7):
-            await message.channel.send('Hola Ray, nomas queria tomarme este minuto para decir que te quiero, eres una gran persona, si estas pasando por un mal momento en tu vida sabe que tienes amigos con quien platicar y se tomarian el tiempo para escucharte. Nos sentimos bendecidos porque te tenemos alrededor de nosotros y esperamos que sigas ahi. Hemos pasado por muchos momentos de risa y estamos agradecidos por ello. Eres la persona que se puede contar para tener unas buenas risas. Tambien vales verga.')
-            
-@client.event
-async def on_message(message): #funcion para devolver mucho texto a una persona despues de 15 mensajes
+            await message.channel.send('Hola Ray, nomas queria tomarme este minuto para decir que te quiero, eres una gran persona, si estas pasando por un mal momento en tu vida sabe que tienes amigos con quien platicar y se tomarian el tiempo para escucharte. Nos sentimos bendecidos porque te tenemos alrededor de nosotros y esperamos que sigas ahi. Hemos pasado por muchos momentos de risa y estamos agradecidos por ello. Eres la persona que se puede contar para tener unas buenas risas.')
+        
+    
+    #Codigo para el texto de Mucho Texto
     global id
     global counter
                         
@@ -77,12 +138,15 @@ async def on_message(message): #funcion para devolver mucho texto a una persona 
         counter = 1
         
     id = message.author.id
-
+    
+    await client.process_commands(message)
+    
+    
+    
+    #-----------------------------------------------
             
-    
-    
 
-client.run('NzIwMjc2MzMzNzgyODkyNjM2.XuDnnA.cBKj3PzfhKatqabsZwooW69qhyk')
+client.run('NzIwMjc2MzMzNzgyODkyNjM2.XuDnnA.LVhJ0Gp7e2OUp1RZQs7U8Ankzoc')
 
 #Id de ray  324383693248659456
 #Id de jesusu  166675689506996234
